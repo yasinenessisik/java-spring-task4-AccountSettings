dbbd2b4658383caabfb3bd1d1c9188200171328b
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-04-16 17:04:13 +0300

    Initial commit

---
+++ src/main/java/com/javaspringtask4AccountSettings/javaspringtask4AccountSettings/JavaSpringTask4AccountSettingsApplication.java
@@ -0,0 +1,13 @@
+package com.javaspringtask4AccountSettings.javaspringtask4AccountSettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTask4AccountSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTask4AccountSettingsApplication.class, args);
+	}
+
+}
